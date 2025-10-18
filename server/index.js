be7f0f661b1adf08d5b6d564bd5aef5e5afb280a
suraj-username <surajfb2004@gmail.com>
2025-10-18 19:43:10 +0530

    feat(auth): configure passport for google oauth

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const session = require('express-session');
+const passport = require('passport');
 const connectDB = require('./config/db');
 
-// Load the environment variables from the .env file.
+// Load the environment variables from the .env file
 dotenv.config();
+
+// We pass the `passport` instance to our configuration file and we configure the instance
+require('./config/passport')(passport);
 
 connectDB();
 
 const app = express();
 
-// Access PORT from .env with fallback
+// Session Middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false, // Don't save session if unmodified
+  saveUninitialized: false // Don't create session until something is stored
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session()); // Enables persistent login sessions
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
